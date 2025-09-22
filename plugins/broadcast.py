1b15d6e18eb5f14024a70955e9a9cfd9daed4153
Yosuke775 <priyanshuprasad775@gmail.com>
2025-09-22 17:43:22 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -25,30 +25,33 @@ from bot import Bot
 from config import *
 from helper_func import *
 from database.database import *
+from plugins.VoidXTora import check_admin_or_owner
 
 
-#=====================================================================================##
-
-REPLY_ERROR = "<code>Use this command as a reply to any telegram message without any spaces.</code>"
 
 #=====================================================================================##
+# Credits: @VOIDXTORA
+# Maintained by: @Mythic_Bots
+# Support: @MythicBot_Support
 
+#=====================================================================================##
+REPLY_ERROR = "<code>Use this command as a reply to any Telegram message.</code>"
 
-@Bot.on_message(filters.private & filters.command('pbroadcast') & admin)
+#=====================================================================================##
+# Broadcast & Pin
+@Bot.on_message(filters.private & filters.command('pbroadcast'))
 async def send_pin_text(client: Bot, message: Message):
+    if not await check_admin_or_owner(message):
+        return
+
     if message.reply_to_message:
         query = await db.full_userbase()
         broadcast_msg = message.reply_to_message
-        total = 0
-        successful = 0
-        blocked = 0
-        deleted = 0
-        unsuccessful = 0
+        total = successful = blocked = deleted = unsuccessful = 0
 
         pls_wait = await message.reply("<i>ʙʀᴏᴀᴅᴄᴀꜱᴛ ᴘʀᴏᴄᴇꜱꜱɪɴɢ....</i>")
         for chat_id in query:
             try:
-                # Send and pin the message
                 sent_msg = await broadcast_msg.copy(chat_id)
                 await client.pin_chat_message(chat_id=chat_id, message_id=sent_msg.id, both_sides=True)
                 successful += 1
@@ -63,8 +66,7 @@ async def send_pin_text(client: Bot, message: Message):
             except InputUserDeactivated:
                 await db.del_user(chat_id)
                 deleted += 1
-            except Exception as e:
-                print(f"Failed to send or pin message to {chat_id}: {e}")
+            except Exception:
                 unsuccessful += 1
             total += 1
 
@@ -79,23 +81,22 @@ Unsuccessful: <code>{unsuccessful}</code>"""
         return await pls_wait.edit(status)
 
     else:
-        msg = await message.reply("Reply to a message to broadcast and pin it.")
+        msg = await message.reply(REPLY_ERROR)
         await asyncio.sleep(8)
         await msg.delete()
 
+
 #=====================================================================================##
-
-
-@Bot.on_message(filters.private & filters.command('broadcast') & admin)
+# Simple Broadcast
+@Bot.on_message(filters.private & filters.command('broadcast'))
 async def send_text(client: Bot, message: Message):
+    if not await check_admin_or_owner(message):
+        return
+
     if message.reply_to_message:
         query = await db.full_userbase()
         broadcast_msg = message.reply_to_message
-        total = 0
-        successful = 0
-        blocked = 0
-        deleted = 0
-        unsuccessful = 0
+        total = successful = blocked = deleted = unsuccessful = 0
 
         pls_wait = await message.reply("<i>ʙʀᴏᴀᴅᴄᴀꜱᴛ ᴘʀᴏᴄᴇꜱꜱɪɴɢ....</i>")
         for chat_id in query:
@@ -114,7 +115,6 @@ async def send_text(client: Bot, message: Message):
                 deleted += 1
             except:
                 unsuccessful += 1
-                pass
             total += 1
 
         status = f"""<b><u>ʙʀᴏᴀᴅᴄᴀꜱᴛ...</u>
@@ -132,44 +132,31 @@ Unsuccessful: <code>{unsuccessful}</code></b>"""
         await asyncio.sleep(8)
         await msg.delete()
 
+
 #=====================================================================================##
-# Don't Remove Credit @CodeFlix_Bots, @rohit_1888
-# Ask Doubt on telegram @CodeflixSupport
-#
-# Copyright (C) 2025 by Codeflix-Bots@Github, < https://github.com/Codeflix-Bots >.
-#
-# This file is part of < https://github.com/Codeflix-Bots/FileStore > project,
-# and is released under the MIT License.
-# Please see < https://github.com/Codeflix-Bots/FileStore/blob/master/LICENSE >
-#
-# All rights reserved.
-#
-
-# broadcast with auto-del
-
-@Bot.on_message(filters.private & filters.command('dbroadcast') & admin)
+# Broadcast with auto-delete
+@Bot.on_message(filters.private & filters.command('dbroadcast'))
 async def delete_broadcast(client: Bot, message: Message):
+    if not await check_admin_or_owner(message):
+        return
+
     if message.reply_to_message:
         try:
-            duration = int(message.command[1])  # Get the duration in seconds
+            duration = int(message.command[1])  # Duration in seconds
         except (IndexError, ValueError):
-            await message.reply("<b>Pʟᴇᴀsᴇ ᴜsᴇ ᴀ ᴠᴀʟɪᴅ ᴅᴜʀᴀᴛɪᴏɴ ɪɴ sᴇᴄᴏɴᴅs.</b> Usᴀɢᴇ: /dbroadcast {duration}")
+            await message.reply("<b>Provide a valid duration in seconds.</b>\nUsage: /dbroadcast {duration}")
             return
 
         query = await db.full_userbase()
         broadcast_msg = message.reply_to_message
-        total = 0
-        successful = 0
-        blocked = 0
-        deleted = 0
-        unsuccessful = 0
+        total = successful = blocked = deleted = unsuccessful = 0
 
         pls_wait = await message.reply("<i>Broadcast with auto-delete processing....</i>")
         for chat_id in query:
             try:
                 sent_msg = await broadcast_msg.copy(chat_id)
-                await asyncio.sleep(duration)  # Wait for the specified duration
-                await sent_msg.delete()  # Delete the message after the duration
+                await asyncio.sleep(duration)
+                await sent_msg.delete()
                 successful += 1
             except FloodWait as e:
                 await asyncio.sleep(e.x)
@@ -185,33 +172,25 @@ async def delete_broadcast(client: Bot, message: Message):
                 deleted += 1
             except:
                 unsuccessful += 1
-                pass
             total += 1
 
-        status = f"""<b><u>Bʀᴏᴀᴅᴄᴀsᴛɪɴɢ ᴡɪᴛʜ Aᴜᴛᴏ-Dᴇʟᴇᴛᴇ...</u>
+        status = f"""<b><u>Broadcasting with Auto-Delete</u></b>
 
 Total Users: <code>{total}</code>
 Successful: <code>{successful}</code>
 Blocked Users: <code>{blocked}</code>
 Deleted Accounts: <code>{deleted}</code>
-Unsuccessful: <code>{unsuccessful}</code></b>"""
+Unsuccessful: <code>{unsuccessful}</code>"""
 
         return await pls_wait.edit(status)
 
     else:
-        msg = await message.reply("Pʟᴇᴀsᴇ ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴛᴏ ʙʀᴏᴀᴅᴄᴀsᴛ ɪᴛ ᴡɪᴛʜ Aᴜᴛᴏ-Dᴇʟᴇᴛᴇ.")
+        msg = await message.reply(REPLY_ERROR)
         await asyncio.sleep(8)
         await msg.delete()
 
-
-# Don't Remove Credit @CodeFlix_Bots, @rohit_1888
-# Ask Doubt on telegram @CodeflixSupport
-#
-# Copyright (C) 2025 by Codeflix-Bots@Github, < https://github.com/Codeflix-Bots >.
-#
-# This file is part of < https://github.com/Codeflix-Bots/FileStore > project,
-# and is released under the MIT License.
-# Please see < https://github.com/Codeflix-Bots/FileStore/blob/master/LICENSE >
-#
-# All rights reserved.
-#
+#=====================================================================================##
+# Credits: @VOIDXTORA
+# Maintained by: @Mythic_Bots
+# Support: @MythicBot_Support
+#=====================================================================================##
