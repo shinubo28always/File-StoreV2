073ed08d8f7ff5d08ef30215ac0a4346f358df0a
Yosuke775 <priyanshuprasad775@gmail.com>
2025-10-28 21:08:01 +0530

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -160,3 +160,12 @@ async def get_admins(client: Client, message: Message):
 
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data="close")]])
     await pro.edit(f"<b>⚡ Current Admin List:</b>\n\n{admin_list}", reply_markup=reply_markup)
+
+#=====================================================================================##
+# Credits:- @VoidXTora
+# Maintained by: Mythic_Bots
+# Support: @MythicBot_Support
+#=====================================================================================##
+This file is part of MythicBots Project.
+Base Repo Codeflixbot.
+"""
